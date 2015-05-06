5d3e0ec3b5da4d586e78253fe67b2cfa0c5ab34d
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-06 11:09:04 -0700

    starting Instagram API

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	//Configuration for PHP server
+	set_time_limit(0);
+	ini_set('default_socket_timeout', 300);
+	//starts session
+	session_start();
+
+	//Make Constants using define
+	define('client_id', );
+	define('client_secret', );
+	define('redirectURI', );
+	define('ImageDirectory', 'pics/');
+
+?>
