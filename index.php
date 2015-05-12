59919710155d20f75afb908dde4c33aadff94ea1
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-11 20:16:55 -0700

    milestone 4. unknown error

--- index.php
+++ index.php
@@ -6,28 +6,28 @@
 	session_start();
 
 	//Make Constants using define
-	define('client_ID', '025e77c43f93484fb23e475d3530c15c');
-	define('client_Secret', '952be6ecb7d941f89de536e5fdd9d75e');
+	define('clientID', '025e77c43f93484fb23e475d3530c15c');
+	define('clientSecret', '952be6ecb7d941f89de536e5fdd9d75e');
 	define('redirectURI', 'http://localhost/maxapi/index.php');
 	define('ImageDirectory', 'pics/');
 
 ?>
 
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="description" content="">
-	<meta name="viewport" content="width098, intitial-scale = 1">
-	<title>InstApi</title>
-	<meta rel="stylesheet" href="css/style.css">
-	<meta rel="author" href="humans.txt">
-</head>
-<body>
-	<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
-		 After getting aprroval we are now going to have the information so that we can play with it.
-	 -->
-	<a href="https:api.instagram/oauth/authorize/?client_id= <?php echo client_ID; ?> &redirect_uri= <?php echo redirectURI; ?> &response_type=code">LOGIN</a>
-	<script src="js/main.js"></script>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="">
+		<meta name="viewport" content="width098, intitial-scale = 1">
+		<title>InstApi</title>
+		<meta rel="stylesheet" href="css/style.css">
+		<meta rel="author" href="humans.txt">
+	</head>
+	<body>
+		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
+			 After getting aprroval we are now going to have the information so that we can play with it.
+		 -->
+		<a href="https:api.instagram.com/oauth/authorize/?client_id = <?php echo clientID; ?> & redirect_uri = <?php echo redirectURI; ?> & response_type=code">LOGIN</a>
+		<script src="js/main.js"></script>
+	</body>
 </html>
