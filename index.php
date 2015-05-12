e2e8ae1044625bb311fca8950d8f29c7a192a3d8
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-12 11:25:42 -0700

    milestone 9

--- index.php
+++ index.php
@@ -11,6 +11,20 @@
 	define('redirectURI', 'http://localhost/maxapi/index.php');
 	define('ImageDirectory', 'pics/');
 
+	//connects to instagram
+	function connectToInstagram($url){
+		$ch = curl_init();
+		curl_setopt_array($ch, array{
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => false,
+			CURLOPT_SSL_VERIFYHOST => 2,
+		)};
+		$result = curl_exec($ch);
+		//closes curl handler
+		curl_close($ch);
+		return $result;
+	}
 
 	if (isset($_GET['code'])) {
 		$code = ($_GET['code']);
