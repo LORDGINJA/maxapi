443851cb732da4913d4c0c9841401fe7e4433bbe
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-11 21:56:45 -0700

    milestone 5

--- index.php
+++ index.php
@@ -11,6 +11,17 @@
 	define('redirectURI', 'http://localhost/maxapi/index.php');
 	define('ImageDirectory', 'pics/');
 
+
+	if (isset($_GET['code'])) {
+		$code = ($_GET['code']);
+		$url = 'https://api.instagram.com/oauth/access_token';
+		$access_token_settings = array('client_id' => clientID,
+									   	'client_secret' => clientSecret,
+										'grant_type' => 'authorization_code',
+										'redirect_uri' => redirectURI, 
+										'code' => $code);
+	}
+
 ?>
 
 <!DOCTYPE html>
