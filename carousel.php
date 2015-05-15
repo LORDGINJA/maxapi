94dc06ff9f08e3c6c45a10c21a120242449de7e4
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-15 08:09:21 -0700

    almost got carousel working

--- carousel.php
+++ carousel.php
@@ -1,99 +1,67 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link type="text/css" rel="stylesheet"  href="css/bootstrap.css">
-	<link type="text/css" rel="stylesheet"  href="css/bootstrap.css.map">
-	<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css">
-	<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css.map">
-	<link type="text/css" rel="stylesheet"  href="css/main.css">
-	<meta charset="utf-8">
-	<title></title>
-</head>
-<body>
-	<header>
-		My Photos
-	</header>
-	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" >
-	<ol class="carousel-indicators">
-		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="4"></li>
-	</ol>
-	<div class="carousel-inner" >
-		<div class="item active" class="carsel">
-			<?php
-				require_once(__DIR__ ."/pics.php");
-			?>
+	<head>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link type="text/css" rel="stylesheet"  href="css/bootstrap.css">
+		<link type="text/css" rel="stylesheet"  href="css/bootstrap.css.map">
+		<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css">
+		<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css.map">
+		<link type="text/css" rel="stylesheet"  href="css/main.css">
+		<meta charset="utf-8">
+		<title>InstApi</title>
+	</head>
+	<body>
+		<header>
+			My Photos
+		</header>
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" >
+			<ol class="carousel-indicators">
+				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+				<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+			</ol>
+			<div class="carousel-inner carsel" >
+				<div class="item active" class="carsel">
+					<?php
+						require_once(__DIR__ ."/piks.php");
+					?>
+				</div>
+				<div class="item carsel">
+					<?php
+						require_once(__DIR__ ."/piks.php");
+					?>
+				</div>
+				<div class="item carsel">
+					<?php
+						require_once(__DIR__ ."/piks.php");
+					?>
+				</div>
+				<div class="item carsel">
+					<?php
+						require_once(__DIR__ ."/piks.php");
+					?>
+				</div>
+				<div class="item carsel">
+					<?php
+						require_once(__DIR__ ."/piks.php");
+					?>
+				</div>
+			</div>
+			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
-		<div class="item active" class="carsel">
-			<?php
-				require_once(__DIR__ ."/pics.php");
-			?>
-		</div>
-		<div class="item active" class="carsel">
-			<?php
-				require_once(__DIR__ ."/pics.php");
-			?>
-		</div>
-		<div class="item active" class="carsel">
-			<?php
-				require_once(__DIR__ ."/pics.php");
-			?>
-		</div>
-		<div class="item active" class="carsel">
-			<?php
-				require_once(__DIR__ ."/pics.php");
-			?>
-		</div>
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left"></span>
-		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right"></span>
-		</a>
-	</div>
-</div>
-</body>
+	</body>
+	<script src="js/main.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </html>
 
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
